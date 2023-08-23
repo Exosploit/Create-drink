3507c7f8780c5f292500c56b16776f43c2096b27
Exosploit <128239207+Exosploit@users.noreply.github.com>
2023-08-23 16:42:58 +0200

    Filtering system v1.1 + Buttons

--- create/script.js
+++ create/script.js
@@ -15,14 +15,11 @@ document.addEventListener('DOMContentLoaded', function() {
     filterElements.forEach(filter => {
         filter.addEventListener('click', () => {
             filter.classList.toggle('active');
-            sortFilters();
         });
     });
-
-    sortFilters();
 });
 
-function sortFilters() {
+/* function sortFilters() {
     const filtersContainer = document.querySelector('.filters-container');
     const filterElements = Array.from(document.querySelectorAll('.filters'));
 
@@ -33,44 +30,50 @@ function sortFilters() {
 
     activeFilters.forEach(filter => filtersContainer.appendChild(filter));
     inactiveFilters.forEach(filter => filtersContainer.appendChild(filter));
-}
-
+} */
 document.addEventListener('DOMContentLoaded', function() {
     const filtersContainer = document.querySelector('.filters-container-size');
     const filterElements = Array.from(document.querySelectorAll('.filters-size'));
 
-    const activeFilters = filterElements.filter(filter => filter.classList.contains('active'));
-    const inactiveFilters = filterElements.filter(filter => !filter.classList.contains('active'));
-
-    filtersContainer.innerHTML = '';
-
-    activeFilters.forEach(filter => filtersContainer.appendChild(filter));
-    inactiveFilters.forEach(filter => filtersContainer.appendChild(filter));
-
     filterElements.forEach(filter => {
         filter.addEventListener('click', () => {
-            filter.classList.toggle('active');
-            sortFilters();const selectedFilters = Array.from(document.querySelectorAll('.filters-size.active'));
-            if (selectedFilters.length === 0) {
+            if (filter.classList.contains('active')) {
+                if (getActiveFilters().length > 1) {
+                    filter.classList.remove('active');
+                }
+            } else {
                 filter.classList.add('active');
-                sortFilters();
             }
+
         });
     });
+
+    function getActiveFilters() {
+        return filterElements.filter(filter => filter.classList.contains('active'));
+    }
+
+    function updateFiltersContainer() {
+        filtersContainer.innerHTML = '';
+        getActiveFilters().forEach(filter => filtersContainer.appendChild(filter));
+        filterElements.filter(filter => !filter.classList.contains('active')).forEach(filter => filtersContainer.appendChild(filter));
+    }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function filter() {
     
     class DrinkType {
     
-        constructor(relevance, litres, price, carbonated, alcoholic, size) { 
+        constructor(relevance, litres, price, size, carbonated, alcoholic, notcarbonated, notalcoholic) { 
             this.relevance = relevance;
             this.litres = litres;
             this.price = price;
             this.ppl = price / litres;
+            this.size = size;
             this.carbonated = carbonated;
             this.alcoholic = alcoholic;
-            this.size = size;
+            this.notcarbonated = notcarbonated;
+            this.notalcoholic = notalcoholic;
         }
     
     }
@@ -85,40 +88,44 @@ document.addEventListener('DOMContentLoaded', function() {
         minlitres: 1,
         maxlitres: 7,
         carbonated: true,
-        alcoholic: true,
+        alcoholic: false,
+        notcarbonated: false, /* reverse from carb except if all is on */
+        notalcoholic: true, /* reverse from carb except if all is on */
         small: true,
         medium: true,
         large: true
     };
 
-    const drinksIsFilteredCondition = (appliedFilters, DrinkType) => 
-    (appliedFilters.minlitres <= DrinkType.litres && appliedFilters.maxlitres >= DrinkType.litres) &&
-    (appliedFilters.minprice <= DrinkType.price && appliedFilters.maxprice >= DrinkType.price) &&
-    ((appliedFilters.small == true && DrinkType.size == 1) || 
-    (appliedFilters.medium == true && DrinkType.size == 2) || 
-    (appliedFilters.large == true && DrinkType.size == 3)) &&
-    (appliedFilters.carbonated == DrinkType.carbonated) && 
-    (appliedFilters.alcoholic == DrinkType.alcoholic)
+    const drinksIsFilteredCondition = (appliedFilters, drink) => 
+    (appliedFilters.minlitres <= drink.litres && appliedFilters.maxlitres >= drink.litres) &&
+    (appliedFilters.minprice <= drink.price && appliedFilters.maxprice >= drink.price) &&
+    ((appliedFilters.small && drink.size === 1) || 
+    (appliedFilters.medium && drink.size === 2) || 
+    (appliedFilters.large && drink.size === 3)) &&
+    ((appliedFilters.carbonated && drink.carbonated) || 
+    (appliedFilters.notcarbonated && drink.notcarbonated)) && 
+    ((appliedFilters.alcoholic && drink.alcoholic) || 
+    (appliedFilters.notalcoholic && drink.notalcoholic));
     
     // CARBONATED/ALCOHOLIC 0 = no; 1 = yes; 2 = both //
-    // RELEVANCE, LITRES, PRICE, CARBONATED, BOTH CARBONATED AND NOT, ALCOHOLIC, BOTH ALCOHOLIC AND NOT, SIZE //
-    let drinks = [
+    // RELEVANCE, LITRES, PRICE, SIZE, CARBONATED, ALCOHOLIC, NOT CARBONATED, NOT ALCOHOLIC //
+    let drinks = [ 
     
-        new DrinkType(1, 1, 1, (true || false), (true || false), 1), // 100ml can (small)
-        new DrinkType(2, 2, 2, true, false, 1), // 250ml can (medium)
-        new DrinkType(3, 3, 3, true, false, 2), // 500ml can (large)
+        new DrinkType(1, 1, 1, 1, true, true, true, true), // 100ml can (small)
+        new DrinkType(2, 2, 2, 1, true, false, true, true), // 250ml can (medium)
+        new DrinkType(3, 3, 3, 2, true, false, false, true), // 500ml can (large)
         
-        new DrinkType(4, 4, 4, false, false, 2), // 750ml water bottle
-        new DrinkType(5, 3, 3, (true || false), false, 2), // 500ml bottle
-        new DrinkType(6, 3, 5, true, false, 2), // 500ml glass bottle
+        new DrinkType(4, 4, 4, 2, false, false, true, true), // 750ml water bottle
+        new DrinkType(5, 3, 3, 2, true, false, true, false), // 500ml bottle
+        new DrinkType(6, 3, 5, 2, true, false, false, true), // 500ml glass bottle
     
-        new DrinkType(7, 5, 5, (true || false), false, 3), // 1000ml bottle (small)
-        new DrinkType(8, 6, 7, true, false, 3), // 1500ml bottle (medium)
-        new DrinkType(9, 7, 8, true, false, 3), // 2000ml bottle (large)
+        new DrinkType(7, 5, 5, 3, true, false, true, true), // 1000ml bottle (small)
+        new DrinkType(8, 6, 7, 3, true, false, false, true), // 1500ml bottle (medium)
+        new DrinkType(9, 7, 8, 3, true, false, false, true), // 2000ml bottle (large)
         
-        new DrinkType(10, 3, 6, (true || false), true, 2), // 500ml beer bottle
-        new DrinkType(11, 5, 9, (true || false), true, 3), // 1000ml whine bottle
-        new DrinkType(12, 5, 10, (true || false), true, 3) // 1000ml champagne bottle
+        new DrinkType(10, 3, 6, 2, true, true, true, false), // 500ml beer bottle
+        new DrinkType(11, 5, 9, 3, false, true, true, false), // 1000ml whine bottle
+        new DrinkType(12, 5, 10, 3, true, true, true, false) // 1000ml champagne bottle
         
     ]
 
@@ -130,8 +137,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
     function showElement(elementID) {
-        var elementToHide = document.getElementById(elementID);
-        elementToHide.style.display = 'flex';
+        var elementToShow = document.getElementById(elementID);
+        elementToShow.style.display = 'flex';
     }
     
     function renderDrinks(filteredDrinks, unFilteredDrinks)
@@ -155,36 +162,39 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     renderDrinks(getFilteredDrinks(drinks, appliedFilters), getUnFilteredDrinks(drinks, appliedFilters))
+
+    const showAllButton = document.getElementById('showall');
+    const alcoholicFilterButton = document.getElementById('alcoholic');
+    const carbonatedFilterButton = document.getElementById('carbonated');
+    
+    let previousFilters = { ...appliedFilters };
+
+    // FIX THIS!
+    function showAllClick() {
+        
+    }
+
+    // WORKS
+    
+    function alcoholicFilterClick() {
+        appliedFilters.alcoholic = !appliedFilters.alcoholic;
+        appliedFilters.notalcoholic = !appliedFilters.alcoholic;
+        renderDrinks(getFilteredDrinks(drinks, appliedFilters), getUnFilteredDrinks(drinks, appliedFilters))
+    }
+
+    function carbonatedFilterClick() {
+        appliedFilters.carbonated = !appliedFilters.carbonated;
+        appliedFilters.notcarbonated = !appliedFilters.carbonated;
+        renderDrinks(getFilteredDrinks(drinks, appliedFilters), getUnFilteredDrinks(drinks, appliedFilters))
+    }
+
+    // Attach the appropriate click event handlers to each filter
+    showAllButton.addEventListener('click', showAllClick);
+    alcoholicFilterButton.addEventListener('click', alcoholicFilterClick);
+    carbonatedFilterButton.addEventListener('click', carbonatedFilterClick);
 });
 
 // Get each filter element by its ID
-const nonCarbonatedFilter = document.getElementById('non-carbonated');
-const alcoholicFilter = document.getElementById('alcoholic');
-const carbonatedFilter = document.getElementById('carbonated');
-const nonAlcoholicFilter = document.getElementById('non-alcoholic');
-
-// Define click event handling functions for each filter
-function nonCarbonatedFilterClick() {
-    // Add your custom logic for the non-carbonated filter here
-}
-
-function alcoholicFilterClick() {
-    // Add your custom logic for the alcoholic filter here
-}
-
-function carbonatedFilterClick() {
-    // Add your custom logic for the carbonated filter here
-}
-
-function nonAlcoholicFilterClick() {
-    // Add your custom logic for the non-alcoholic filter here
-}
-
-// Attach the appropriate click event handlers to each filter
-nonCarbonatedFilter.addEventListener('click', nonCarbonatedFilterClick);
-alcoholicFilter.addEventListener('click', alcoholicFilterClick);
-carbonatedFilter.addEventListener('click', carbonatedFilterClick);
-nonAlcoholicFilter.addEventListener('click', nonAlcoholicFilterClick);
 
 const styleContainers = document.querySelectorAll('.style-container');
 
@@ -197,7 +207,6 @@ styleContainers.forEach((container) => {
         div.classList.remove('active');
       });
 
-      // ADD SOME TYPE OF MEMORY OF WHICH IS SELECTED
       div.classList.add('active');
     });
   });
@@ -212,18 +221,6 @@ styleDivs.forEach((div) => {
       div.classList.remove('active');
     });
 
-    // ADD SOME TYPE OF MEMORY OF WHICH IS SELECTED
     div.classList.add('active');
   });
 });
-
-
-
-// FILTERING SYSTEM //
-
-// carbonated/alcoholic 0 = no; 1 = yes; 2 = both;
-// size 1 = small; 2 = medium; 3 = large;
-
-
-
-
